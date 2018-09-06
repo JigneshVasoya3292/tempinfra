6ef30f603db976c94c2f965c39bd3960cf20c533
jvasoya <jignesh@credth.io>
2018-09-06 13:33:30 +0000

    start fixes

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ docker-compose -f docker-compose-orderer.yml start
 docker-compose -f docker-compose-mitel-peers.yml start
 docker-compose -f docker-compose-org1-peers.yml start
 docker-compose -f docker-compose-org2-peers.yml start
-docker-compose -f ./mite-node/docker-compose-api.yaml start
+docker-compose -f ./mitel-node/docker-compose-api.yaml start
 docker-compose -f ./org1-node/docker-compose-api.yaml start
 docker-compose -f ./org2-node/docker-compose-api.yaml start

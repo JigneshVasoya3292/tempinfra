eb3a2b7ba3c54d85ec2b95bd5cc0c91649641701
jvasoya <jignesh@credth.io>
2018-09-06 13:31:05 +0000

    start scripts

---
+++ restart.sh
@@ -0,0 +1,7 @@
+docker-compose -f docker-compose-orderer.yml restart
+docker-compose -f docker-compose-mitel-peers.yml restart
+docker-compose -f docker-compose-org1-peers.yml restart
+docker-compose -f docker-compose-org2-peers.yml restart
+docker-compose -f ./mite-node/docker-compose-api.yaml restart
+docker-compose -f ./org1-node/docker-compose-api.yaml restart
+docker-compose -f ./org2-node/docker-compose-api.yaml restart
---
+++ start.sh
@@ -0,0 +1,7 @@
+docker-compose -f docker-compose-orderer.yml start
+docker-compose -f docker-compose-mitel-peers.yml start
+docker-compose -f docker-compose-org1-peers.yml start
+docker-compose -f docker-compose-org2-peers.yml start
+docker-compose -f ./mite-node/docker-compose-api.yaml start
+docker-compose -f ./org1-node/docker-compose-api.yaml start
+docker-compose -f ./org2-node/docker-compose-api.yaml start
